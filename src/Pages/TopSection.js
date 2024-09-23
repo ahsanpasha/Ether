36f77172e3058139cb68a848470ecf563cf49170
ahsanpasha <ahsanfiazpasha@gmail.com>
2024-09-23 14:16:52 +0500

    first commit

--- src/Pages/TopSection.js
+++ src/Pages/TopSection.js
@@ -284,15 +284,7 @@ const TopSection = () => {
                 anchor="left"
                 open={drawerOpen}
                 onClose={handleDrawerToggle}
-                sx={{
-                    '& .MuiPaper-root': {
-                        backgroundColor: '#000000 !important',
-                        color: '#FFFFFF !important',
-                        backgroundImage: 'url(/public/Images/Circle.png) !important',
-                        backgroundRepeat: 'no-repeat !important',
-                        backgroundPosition: '0 0 !important',
-                    }
-                }}
+                classes={{ paper: 'custom-drawer' }} // Add your custom class here
             >
                 <Box sx={{ padding: 2, textAlign: 'center' }} >
                     <img src="/Images/Ether.svg" alt="Drawer Logo" style={{ width: '50%', marginBottom: '16px' }} />
